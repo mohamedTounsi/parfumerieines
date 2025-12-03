396a9540d8ecf8e7aca0799fddf0b60eb84b6233
Mohamed Tounsi <tounsim317@gmail.com>
2025-12-03 13:38:36 +0100

    hh

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
 
+import Hero from "./components/Hero";
+import DownHero from "./components/DownHero";
+import LatestCollection from "./components/LatestCollection";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HeroSection from "./components/Herosection";
 export default function Home() {
   return (
     <div>
-   
+      <Header />
+      <HeroSection/>
+
+      <LatestCollection />
+
+      <Footer />
     </div>
   );
 }
